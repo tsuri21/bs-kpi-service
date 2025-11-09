b507fd3f5e40fa510ae7d5a53b4b7a94fb3b41d4
tsuri21 <git-ts21@mailbox.org>
2025-11-09 20:05:19 +0100

    Add interface port for Project persistence operations

---
+++ src/main/java/de/thws/fiw/bs/kpi/domain/port/out/persistence/ProjectRepository.java
@@ -0,0 +1,18 @@
+package de.thws.fiw.bs.kpi.domain.port.out.persistence;
+
+import de.thws.fiw.bs.kpi.domain.model.Project;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+interface ProjectRepository {
+    UUID create(Project p);
+    Optional<Project> findById(UUID id);
+    Optional<Project> findByUrl(URI repoUrl);
+    List<Project> findAll();
+    boolean existsById(UUID id);
+    void update(Project p);
+    void deleteById(UUID id);
+}
