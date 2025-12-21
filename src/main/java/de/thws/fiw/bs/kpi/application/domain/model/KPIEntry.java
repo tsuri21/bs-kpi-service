7d7053017a4f18ed90925f64c65b26444c3bd6a9
tsuri21 <git-ts21@mailbox.org>
2025-12-22 00:16:08 +0100

    Add KPI assignment reference to KPIEntry

--- src/main/java/de/thws/fiw/bs/kpi/application/domain/model/KPIEntry.java
+++ src/main/java/de/thws/fiw/bs/kpi/application/domain/model/KPIEntry.java
@@ -7,17 +7,23 @@ import java.util.Objects;
 public class KPIEntry {
 
     private final KPIEntryId id;
+    private final KPIAssignmentId kpiAssignmentId;
     private final Instant timestamp;
     private final double value;
 
-    public KPIEntry(KPIEntryId id, Instant timestamp, double value, Clock clock) {
+    public KPIEntry(KPIEntryId id, KPIAssignmentId kpiAssignmentId, Instant timestamp, double value, Clock clock) {
         this.id = Objects.requireNonNull(id, "KPIEntry id must not be null");
+        this.kpiAssignmentId = Objects.requireNonNull(kpiAssignmentId, "KPIAssignment id must not be null");
         this.timestamp = validateTimestamp(timestamp, clock);
         this.value = value;
     }
 
     public KPIEntryId getId() {
         return id;
+    }
+
+    public KPIAssignmentId getKpiAssignmentId() {
+        return kpiAssignmentId;
     }
 
     public Instant getTimestamp() {
