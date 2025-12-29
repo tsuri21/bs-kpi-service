de140906e63771b10f09ba7e94d6e90f4721c88c
tsuri21 <git-ts21@mailbox.org>
2025-12-29 01:12:55 +0100

    Fix wrong check for number of total elements and simplify class to record

--- src/main/java/de/thws/fiw/bs/kpi/application/port/Page.java
+++ src/main/java/de/thws/fiw/bs/kpi/application/port/Page.java
@@ -2,33 +2,14 @@ package de.thws.fiw.bs.kpi.application.port;
 
 import java.util.List;
 
-public class Page<T> {
-    private final List<T> content;
-    private final PageRequest pageRequest;
-    private final double total;
-
-    public Page(List<T> content, PageRequest pageRequest, double total) {
-        this.content = content;
-        this.pageRequest = pageRequest;
-        if (total <= 0) {
-            throw new IllegalArgumentException("Total must be greater than zero");
+public record Page<T>(List<T> content, PageRequest pageRequest, double totalElements) {
+    public Page {
+        if (totalElements < 0) {
+            throw new IllegalArgumentException("Total must not be negative");
         }
-        this.total = total;
-    }
-
-    public List<T> getContent() {
-        return content;
-    }
-
-    public PageRequest getPageRequest() {
-        return pageRequest;
-    }
-
-    public double getTotal() {
-        return total;
     }
 
     public double getTotalPages() {
-        return Math.ceil(total / pageRequest.pageSize());
+        return Math.ceil(totalElements / pageRequest.pageSize());
     }
 }
