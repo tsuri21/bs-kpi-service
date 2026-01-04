823cfbb56484c28fe8f01de5875b2850ce916001
Tilman <tilman.steger@study.thws.de>
2026-01-04 20:41:52 +0100

    Add KPIAssignmentEntity for out adapter

---
+++ src/main/java/de/thws/fiw/bs/kpi/adapter/out/persistence/jpa/entity/KPIAssignmentEntity.java
@@ -0,0 +1,108 @@
+package de.thws.fiw.bs.kpi.adapter.out.persistence.jpa.entity;
+
+import de.thws.fiw.bs.kpi.application.domain.model.Thresholds;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(uniqueConstraints={
+        @UniqueConstraint(columnNames = {"kpi_id", "project_id"})
+})
+public class KPIAssignmentEntity {
+
+    @Id
+    private UUID id;
+
+    @Column(nullable = false)
+    private double green;
+
+    @Column(nullable = false)
+    private double yellow;
+
+    @Column(nullable = false)
+    private double red;
+
+    @Column(name = "kpi_id", insertable = false, updatable = false)
+    private UUID kpiId;
+
+    @ManyToOne
+    @JoinColumn(name = "kpi_id")
+    private KPIEntity kpiEntity;
+
+    @Column(name = "project_id", nullable = false)
+    private UUID projectId;
+
+
+
+    public KPIAssignmentEntity() {
+    }
+
+    public KPIAssignmentEntity(UUID id, double green, double yellow, double red, UUID kpiId, UUID projectId) {
+        this.id = id;
+        this.green = green;
+        this.yellow = yellow;
+        this.red = red;
+        this.kpiId = kpiId;
+        this.projectId = projectId;
+    }
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public double getGreen() {
+        return green;
+    }
+
+    public void setGreen(double green) {
+        this.green = green;
+    }
+
+    public double getYellow() {
+        return yellow;
+    }
+
+    public void setYellow(double yellow) {
+        this.yellow = yellow;
+    }
+
+    public double getRed() {
+        return red;
+    }
+
+    public void setRed(double red) {
+        this.red = red;
+    }
+
+
+    public UUID getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(UUID projectId) {
+        this.projectId = projectId;
+    }
+
+
+    public KPIEntity getKpiEntity() {
+        return kpiEntity;
+    }
+
+    public void setKpiEntity(KPIEntity kpiEntity) {
+        this.kpiEntity = kpiEntity;
+    }
+
+    public UUID getKpiId() {
+        return kpiId;
+    }
+
+    public void setKpiId(UUID kpiId) {
+        this.kpiId = kpiId;
+    }
+}
