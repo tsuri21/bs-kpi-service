8f152e1fd078f3048dd8915c64df0cb05c29798b
Tilman <tilman.steger@study.thws.de>
2026-01-05 15:40:03 +0100

    Adjusted Test to latest changes

--- src/test/java/de/thws/fiw/bs/kpi/application/domain/model/KPIAssignmentTest.java
+++ src/test/java/de/thws/fiw/bs/kpi/application/domain/model/KPIAssignmentTest.java
@@ -10,12 +10,12 @@ class KPIAssignmentTest {
     void init_anyArgumentNull_throwsException() {
         KPIAssignmentId id = KPIAssignmentId.newId();
         Thresholds thresholds = Thresholds.forDestination(TargetDestination.INCREASING, 50, 30, 15);
-        KPIId kpiId = KPIId.newId();
+        KPI kpi = new KPI(KPIId.newId(), new Name("Test"), TargetDestination.DECREASING);
         ProjectId projectId = ProjectId.newId();
 
-        assertThrows(NullPointerException.class, () -> new KPIAssignment(null, thresholds, kpiId, projectId));
-        assertThrows(NullPointerException.class, () -> new KPIAssignment(id, null, kpiId, projectId));
+        assertThrows(NullPointerException.class, () -> new KPIAssignment(null, thresholds, kpi, projectId));
+        assertThrows(NullPointerException.class, () -> new KPIAssignment(id, null, kpi, projectId));
         assertThrows(NullPointerException.class, () -> new KPIAssignment(id, thresholds, null, projectId));
-        assertThrows(NullPointerException.class, () -> new KPIAssignment(id, thresholds, kpiId, null));
+        assertThrows(NullPointerException.class, () -> new KPIAssignment(id, thresholds, kpi, null));
     }
 }
