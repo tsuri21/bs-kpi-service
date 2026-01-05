f7ab91655359787b96158751a3babb86ed5d2738
Tilman <tilman.steger@study.thws.de>
2026-01-05 15:39:43 +0100

    Change attribute KPIId to KPI domain model to make later evaluation of a KPI easier

--- src/main/java/de/thws/fiw/bs/kpi/application/domain/model/KPIAssignment.java
+++ src/main/java/de/thws/fiw/bs/kpi/application/domain/model/KPIAssignment.java
@@ -5,13 +5,13 @@ import java.util.Objects;
 public class KPIAssignment {
     private final KPIAssignmentId id;
     private final Thresholds thresholds;
-    private final KPIId kpiId;
+    private final KPI kpi;
     private final ProjectId projectId;
 
-    public KPIAssignment(KPIAssignmentId id, Thresholds thresholds, KPIId kpiId, ProjectId projectId) {
+    public KPIAssignment(KPIAssignmentId id, Thresholds thresholds, KPI kpi, ProjectId projectId) {
         this.id = Objects.requireNonNull(id, "KPIAssignment id must not be null");
         this.thresholds = Objects.requireNonNull(thresholds, "Thresholds must not be null");
-        this.kpiId = Objects.requireNonNull(kpiId, "KPI id must not be null");
+        this.kpi = Objects.requireNonNull(kpi, "KPI must not be null");
         this.projectId = Objects.requireNonNull(projectId, "Project id must not be null");
     }
 
@@ -23,8 +23,8 @@ public class KPIAssignment {
         return thresholds;
     }
 
-    public KPIId getKpiId() {
-        return kpiId;
+    public KPI getKpi() {
+        return kpi;
     }
 
     public ProjectId getProjectId() {
