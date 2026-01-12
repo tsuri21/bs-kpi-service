1be45e420ab00b8a7f976d40ee63ec511db7acc4
tsuri21 <git-ts21@mailbox.org>
2026-01-12 17:42:39 +0100

    Add domain service for KPIEntry

---
+++ src/main/java/de/thws/fiw/bs/kpi/application/domain/service/KPIEntryService.java
@@ -0,0 +1,43 @@
+package de.thws.fiw.bs.kpi.application.domain.service;
+
+import de.thws.fiw.bs.kpi.application.domain.exception.ResourceNotFoundException;
+import de.thws.fiw.bs.kpi.application.domain.model.kpiAssignment.KPIAssignmentId;
+import de.thws.fiw.bs.kpi.application.domain.model.kpiEntry.KPIEntry;
+import de.thws.fiw.bs.kpi.application.domain.model.kpiEntry.KPIEntryId;
+import de.thws.fiw.bs.kpi.application.port.Page;
+import de.thws.fiw.bs.kpi.application.port.PageRequest;
+import de.thws.fiw.bs.kpi.application.port.in.KPIEntryUseCase;
+import de.thws.fiw.bs.kpi.application.port.out.KPIEntryRepository;
+import jakarta.inject.Inject;
+
+import java.time.Instant;
+import java.util.Optional;
+
+public class KPIEntryService implements KPIEntryUseCase {
+
+    @Inject
+    KPIEntryRepository kpiEntryRepository;
+
+    @Override
+    public Optional<KPIEntry> readById(KPIEntryId id) {
+        return kpiEntryRepository.findById(id);
+    }
+
+    @Override
+    public Page<KPIEntry> readAll(KPIAssignmentId id, Instant from, Instant to, PageRequest pageRequest) {
+        return kpiEntryRepository.findByFilter(id, from, to, pageRequest);
+    }
+
+    @Override
+    public void create(KPIEntry kpiEntry) {
+        kpiEntryRepository.save(kpiEntry);
+    }
+
+    @Override
+    public void delete(KPIEntryId id) {
+        kpiEntryRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("KPIEntry", id));
+
+        kpiEntryRepository.delete(id);
+    }
+}
