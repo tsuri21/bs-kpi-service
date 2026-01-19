204a830700a4934cd4a5b06ac7ef57cfd1f98ced
tsuri21 <git-ts21@mailbox.org>
2026-01-19 15:04:12 +0100

    Add constants for role names to make usage in adapter easier

--- src/main/java/de/thws/fiw/bs/kpi/application/domain/model/user/Role.java
+++ src/main/java/de/thws/fiw/bs/kpi/application/domain/model/user/Role.java
@@ -1,5 +1,9 @@
 package de.thws.fiw.bs.kpi.application.domain.model.user;
 
 public enum Role {
-    ADMIN, TECHNICAL_USER, MEMBER
+    ADMIN, TECHNICAL_USER, MEMBER;
+
+    public static final String ADMIN_ROLE = "ADMIN";
+    public static final String TECH_USER_ROLE = "TECHNICAL_USER";
+    public static final String MEMBER_ROLE = "MEMBER";
 }
